8f6c7b966bd7492849979d0267d3002cc0eef80d
Cristian Nascimento <Cristian-Nascimento>
2022-11-24 10:58:34 -0300

    fix: add webpack

--- bundle.js
+++ bundle.js
@@ -1,22 +1,14 @@
-"use strict";
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); Object.defineProperty(Constructor, "prototype", { writable: false }); return Constructor; }
-var Pessoa = /*#__PURE__*/function () {
-  function Pessoa() {
-    _classCallCheck(this, Pessoa);
-  }
-  _createClass(Pessoa, [{
-    key: "falar",
-    value: function falar() {
-      console.log('ENACOM');
-    }
-  }, {
-    key: "comer",
-    value: function comer() {
-      console.log("Estou comendo");
-    }
-  }]);
-  return Pessoa;
-}();
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	
+/******/ 	
+/******/ })()
+;
